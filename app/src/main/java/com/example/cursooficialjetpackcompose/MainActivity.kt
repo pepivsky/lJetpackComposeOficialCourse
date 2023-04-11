a3c519d49039491ddabf4da39847f89324f98913
pepivsky <ironglas68@gmail.com>
2023-04-10 23:10:08 -0600

    starter example

--- app/src/main/java/com/example/cursooficialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursooficialjetpackcompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.cursooficialjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursooficialjetpackcompose.ui.theme.CursoOficialJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,15 +26,58 @@ class MainActivity : ComponentActivity() {
             CursoOficialJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithImage(
+                        getString(R.string.happy_birthday_text),
+                        getString(R.string.signature_text)
+                    )
                 }
             }
         }
     }
 }
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = message, fontSize = 36.sp, modifier = Modifier.padding(top = 16.dp))
+        //Spacer(modifier = Modifier.size(16.dp))
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun BirthdayCardPreview() {
+    CursoOficialJetpackComposeTheme {
+        BirthdayGreetingWithImage(
+            stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text)
+        )
+    }
+}
+/*
 
 @Composable
 fun Greeting(name: String) {
@@ -40,4 +90,4 @@ fun DefaultPreview() {
     CursoOficialJetpackComposeTheme {
         Greeting("Android")
     }
-}
+}*/
